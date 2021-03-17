603c6072981dd26c9daba16892d87f7f9c347043
athanri <piercejord@gmail.com>
2021-03-17 16:27:20 +0000

    Completed
    AD

--- src/App.js
+++ src/App.js
@@ -7,32 +7,19 @@ import {faImage, faThumbsDown, faThumbsUp, faMoneyCheckAlt, faSearchDollar} from
 class App extends Component {
   state = { 
             isLoading: false, 
-            invoices: [
-              {
-                "id" : "100",
-                "Vendor": "Hankook",
-                "Amount": "€1800",
-                "Date" : "08/21/2019"
-              },
-              {
-                "id" : "101",
-                "Vendor": "HP",
-                "Amount": "€100",
-                "Date" : "08/21/2019"
-              },
-              {
-                "id" : "102",
-                "Vendor": "SAP",
-                "Amount": "€800",
-                "Date" : "21/08/2021"
-              }
-            ]
+            invoices: []
   }
 
   remove(id) {
-    let updatedInvoices = [...this.state.invoices].filter(i => i.id !==id);
+    let updatedInvoices = [...this.state.invoices].filter(i => i.Id !==id);
 
     this.setState({invoices: updatedInvoices});
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://c2jmu36stf.execute-api.eu-west-1.amazonaws.com/Dev');
+    const body = await response.json();
+    this.setState({invoices: body, isLoading: false});
   }
 
   render() {
@@ -43,16 +30,16 @@ class App extends Component {
           return (<div>Loading...</div>)
 
         let invoices = allInvoices.map( invoice => 
-          <tr key={invoice.id}>
+          <tr key={invoice.Id}>
             <td>{invoice.Vendor}</td>
             <td>{invoice.Amount}</td>
-            <td>{invoice.id}</td>
+            <td>{invoice.Id}</td>
             <td>{invoice.Date}</td>
-            <td><Button className="btn btn-large btn-success" onClick={() => this.remove(invoice.id)}><FontAwesomeIcon icon={faThumbsUp} /> OK </Button></td>
-            <td><Button className="btn btn-large btn-danger" onClick={() => this.remove(invoice.id)}><FontAwesomeIcon icon={faThumbsDown} /> NOK </Button></td>
-            <td><Button className="btn btn-large btn-info" onClick={() => this.remove(invoice.id)}><FontAwesomeIcon icon={faMoneyCheckAlt} /> 50% </Button></td>
-            <td><Button className="btn btn-large btn-warning" onClick={() => this.remove(invoice.id)}><FontAwesomeIcon icon={faSearchDollar} /> ?? </Button></td>
-            <td><Button className="btn btn-large btn-primary" onClick={() => this.remove(invoice.id)}><FontAwesomeIcon icon={faImage} /> Image </Button></td>
+            <td><Button className="btn btn-large btn-success" onClick={() => this.remove(invoice.Id)}><FontAwesomeIcon icon={faThumbsUp} /> OK </Button></td>
+            <td><Button className="btn btn-large btn-danger" onClick={() => this.remove(invoice.Id)}><FontAwesomeIcon icon={faThumbsDown} /> NOK </Button></td>
+            <td><Button className="btn btn-large btn-info" onClick={() => this.remove(invoice.Id)}><FontAwesomeIcon icon={faMoneyCheckAlt} /> 50% </Button></td>
+            <td><Button className="btn btn-large btn-warning" onClick={() => this.remove(invoice.Id)}><FontAwesomeIcon icon={faSearchDollar} /> ?? </Button></td>
+            <td><Button className="btn btn-large btn-primary" onClick={() => this.remove(invoice.Id)}><FontAwesomeIcon icon={faImage} /> Image </Button></td>
           </tr>  
         )
         
@@ -74,13 +61,13 @@ class App extends Component {
                       <th scope="row">Amount</th>
                       <th scope="row">Invoice #</th>
                       <th scope="row">Date</th>
-                      <th scope="row" colspan="4">Actions</th>
+                      <th scope="row" colSpan="4">Actions</th>
                       <th scope="row">Image</th>
                     </tr>
                   </thead>
 
                   <tbody>
-                    { this.state.invoices.length === 0 ? <td colspan="9">All Done!</td> : invoices}
+                    { this.state.invoices.length === 0 ? <tr><td colSpan="9">All Done!</td></tr> : invoices}
                   </tbody>
                 </Table>
               </div>
